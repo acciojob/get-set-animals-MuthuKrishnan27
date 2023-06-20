12c9b0559d7974089df8e7886d6098dec347fd87
MUTHUKRISHNAN S <77878932+MuthuKrishnan27@users.noreply.github.com>
2023-06-20 11:39:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species = species;
+	}
+	getter(){
+		return this.species;
+	}
+	makeSound(){
+		if(species===Dog){
+			return Dog.bark();
+		}
+		else{
+			return Cat.purr();
+		}
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(){
+		super("Dog");
+	}
+	bark(){
+		console.log("woof")
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	constructor(){
+		super("Cat");
+	}
+	purr(){
+		console.log("purr")
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
